b0d596a0c9e3c269a89a57170414463ab72108bc
talib <imonn439@gmail.com>
2022-11-02 12:41:02 +0530

    water component created at component folder

---
+++ src/Pages/Property/PropertyCitizenEntryForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Screen1 from '../../Components/PropertyComponents/InputScreens/Screen1'
+
+function PropertyCitizenEntryForm() {
+    return (
+        <>
+            <Screen1 />
+        </>
+    )
+}
+
+export default PropertyCitizenEntryForm
--- src/Pages/Property/PropertyRoutes.js
+++ src/Pages/Property/PropertyRoutes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import PropertyCitizenEntryForm from './PropertyCitizenEntryForm'
 import PropertyIndex from './PropertyIndex'
 
 function PropertyRoutes() {
     return (
         <>
             <Routes>
-                <Route path='/property-index' element={<PropertyIndex />} />
+                <Route path='/property-citizen-form' element={<PropertyCitizenEntryForm />} />
             </Routes>
         </>
     )
