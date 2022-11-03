a5767332f3e3b33cc438e9366d8886a82842f4eb
Dipu Singh <dsingh197@gmail.com>
2022-11-03 13:04:38 +0530

    change

---
+++ src/Components/PropertyComponents/InputScreens/Screen9.js
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//    Author - Dipu Kumar Singh
+//    Version - 1.0
+//    Date - 03 november 2022
+//    Revision - 1
+//    Project - JUIDCO
+//    Component  - Screen9
+//    DESCRIPTION - Scrren7 is single input component
+//////////////////////////////////////////////////////////////////////////////////////
+
+import { AiFillInfoCircle } from 'react-icons/ai'
+import { RiArrowDropLeftFill } from 'react-icons/ri'
+//importing Themestyle function to use predefined colors to maintain uniform theme everywhere
+import ThemeStyle from '../../Styles/ThemeStyle'
+
+function Screen9(props) {
+    //destructuring predefined colors to maintain uniform theme everywhere
+    const { bgHeaderColor, titleColor, nextButtonColor, nextBtnHoverColor, backButtonColor, backBtnHoverColor, bgCardColor, bgInfoColor, infoTextColor } = ThemeStyle()
+    return (
+        <>
+            <div className='text-xs font-semibold px-2 mt-4 flex'>
+                <div className="flex-1"><span onClick={props.backFun} className='border-b border-black'><RiArrowDropLeftFill className="inline text-xl" />Back</span></div>
+                <div className="flex-1 text-right"><span className=''>{props?.formIndex} of 10</span></div>
+            </div>
+            <div className='p-2 md:p-10 flex justify-center items-center  overflow-hidden'>
+                <div className={`grid grid-cols-12 ${bgCardColor} shadow-lg w-full md:w-1/3 p-4 py-10 md:p-10`}>
+                    <div className="col-span-12"> <h1 className={`font-bold ${titleColor} text-2xl`}>Provide Landmark</h1></div>
+                    <div className="col-span-12"> <h1 className={` ${titleColor} text-sm mt-2 opacity-40`}>Provide the landmark to help us react the property location easily.</h1></div>
+                    <div className="form-group mb-4 md:mb-6 col-span-12 mt-4">
+                        
+                        <div>
+                            <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Landmark<span className='text-red-500'>*</span></label>
+                            {/* <input type="textarea" /> */}
+                        <textarea name="" id="" rows="3" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none placeholder-gray-300 shadow-md"></textarea>
+                        </div>
+                    </div>
+
+                    <div className="col-span-12 grid grid-cols-12 gap-x-6 mt-6">
+                        <div className="col-span-6"> <button onClick={() => props.backFun()} type="submit" className={`shadow-lg w-full px-6 py-4 ${backButtonColor} text-white font-medium text-xs leading-tight  rounded  ${backBtnHoverColor} hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`}>Back</button></div>
+                        <div className="col-span-6"> <button onClick={() => props.nextFun()} type="submit" className={`shadow-lg w-full px-6 py-4 ${nextButtonColor} text-white font-medium text-xs leading-tight  rounded  ${nextBtnHoverColor} hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`}>Next</button></div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Screen9
