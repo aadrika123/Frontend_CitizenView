39a6c0589487983ab1e6d80e5dead5eb8bb18b64
swati_05 <swatisharma52000@gmail.com>
2022-11-03 18:19:20 +0530

    water citizen view 3/11/2022

--- src/Components/WaterComponent/WaterNewApply/ConnectionDetailScreen.js
+++ src/Components/WaterComponent/WaterNewApply/ConnectionDetailScreen.js
@@ -30,15 +30,25 @@ function ConnectionDetailScreen(props) {
                     <div className={`grid grid-cols-12 ${bgCardColor} shadow-lg w-full md:w-1/3 p-4 py-10 md:p-10`}>
                         <div className="col-span-12"> <h1 className={`font-bold ${titleColor} text-lg`}>Connection Detail</h1></div>
                         <div className="form-group mb-4 md:mb-6 col-span-12 mt-4">
+                            <div className='flex flex-row'>
+                                <div className=" items-center mb-4">
+                                    <input id="option1" type="radio" value="" name="propertyType" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300  dark:bg-gray-700 dark:border-gray-600" />
+                                    <label for="option1" className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-300">Water</label>
+                                </div>
+                                <div className=" items-center mb-4 ml-4">
+                                    <input checked id="option2" type="radio" value="" name="propertyType" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300  dark:bg-gray-700 dark:border-gray-600" />
+                                    <label for="option2" className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-300">Sewerage</label>
+                                </div>
+                            </div>
+
                             <div className="flex items-center mb-4">
                                 <label className="ml-2 text-sm font-medium text-gray-500 ">No. of taps proposed <span className='text-red-600 text-lg'>*</span></label>
                             </div>
                             <div className="flex items-center -mt-3">
                                 <input type='text' className='border bg-gray-50 shadow-md  px-9 ' />
-                              
                             </div>
                             <div className="flex items-center mb-4">
-                            <label className="ml-2 text-sm font-medium text-gray-500 ">Pipe size proposed(in inches)<span className='text-red-600 text-lg'>*</span></label>
+                                <label className="ml-2 text-sm font-medium text-gray-500 ">Pipe size proposed(in inches)<span className='text-red-600 text-lg'>*</span></label>
                             </div>
                             <div className=" -mt-3">
                                 <select className='border bg-gray-50 shadow-md   px-20 text-left text-gray-500 '>
@@ -48,7 +58,7 @@ function ConnectionDetailScreen(props) {
                                     <option>1</option>
                                 </select>
                             </div>
-                          
+
                         </div>
 
                         <div className="col-span-12 grid grid-cols-12 gap-x-6 mt-6">
