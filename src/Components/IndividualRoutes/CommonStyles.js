54e98ce0ce2513e873c0cec538e5edf546befc2f
AnshumanG20 <faaltuno9@gmail.com>
2022-11-03 19:12:54 +0530

    pushed at evening  3/11/2022

--- src/Components/IndividualRoutes/CommonStyles.js
+++ src/Components/IndividualRoutes/CommonStyles.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CommonStyles() {
 
     const styles = {
-        container : " w-full h-screen bg-gray-200",
+        container : " w-full h-screen bg-white",
         boxStyle:"bg-white w-11/12 h-48 mx-auto shadow-md mt-4 shadow-gray-600 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105  duration-300"
 
     }
