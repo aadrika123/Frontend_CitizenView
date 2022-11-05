b4c461905610322d05913323b7ed90b8f283b451
AnshumanG20 <faaltuno9@gmail.com>
2022-11-05 19:06:47 +0530

    pushed on 05/11/2022

--- src/Components/TradeComponents/InputScreens/Screen13.js
+++ src/Components/TradeComponents/InputScreens/Screen13.js
@@ -13,10 +13,19 @@ import { RiArrowDropLeftFill } from 'react-icons/ri'
 //importing Themestyle function to use predefined colors to maintain uniform theme everywhere
 import ThemeStyle from '../../Styles/ThemeStyle';
 import Multiselect from 'multiselect-react-dropdown';
+import CommonStyles from '../../IndividualRoutes/CommonStyles';
+import Info from '../../Common/Info';
+import { useState } from 'react';
 
 function Screen13(props) {
     //destructuring predefined colors to maintain uniform theme everywhere
     const { bgHeaderColor, titleColor, nextButtonColor, nextBtnHoverColor, backButtonColor, backBtnHoverColor, bgCardColor, bgInfoColor, infoTextColor } = ThemeStyle();
+    const { labelStyle, inputStyle } = CommonStyles();
+    const [tocStatusToggle, settocStatusToggle] = useState(false);
+
+    const handleChange = () => {
+
+    }
 
     return (
         <>
@@ -26,13 +35,46 @@ function Screen13(props) {
             </div>
             <div className='p-2 md:p-10 flex justify-center items-center  overflow-hidden'>
                 <div className={`grid grid-cols-12 ${bgCardColor} shadow-lg w-full md:w-1/3 p-4 py-10 md:p-10`}>
-                    <div className="col-span-12"> <h1 className={`font-bold ${titleColor} text-2xl`}>Add Business Owners</h1></div>
-                    <div className="col-span-12"> <h1 className={` ${titleColor} text-sm mt-2 opacity-40`}>..</h1></div>
-                    <div className="form-group mb-4 md:mb-6 col-span-12 mt-4">
+                    <div className="col-span-12"> <h1 className={`font-bold ${titleColor} text-2xl`}>License For Years</h1></div>
+                    <div className="col-span-12"> <h1 className={` ${titleColor} text-sm opacity-40`}>Enter Number of years you require a license For.</h1></div>
+                    <div className="form-group mb-2 md:mb-6 col-span-12 mt-4">
+                        <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Are You Applying For tobacco ?<span className='text-red-500'>*</span></label>
+                        <div className="w-full overflow-auto flex justify-evenly">
+                            <p><input type="radio" name='tocStatus' value="1" className='border h-4 w-4 shadow-lg' onChange={() => settocStatusToggle(true)} /><span className='font-bold'> YES</span></p>
+                            <p><input type="radio" name='tocStatus' value="0" className='border h-4 w-4 shadow-lg' onChange={() => settocStatusToggle(false)} /><span className='font-bold'> No</span></p>
+                        </div>
 
-                        <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Nature Of Business<span className='text-red-500'>*</span></label>
-                        <div className="w-full overflow-auto h-36">
-                          jhlkjlj 13
+                    </div>
+
+                    <div className={`${tocStatusToggle ? '' : 'hidden'} form-group mb-4 md:mb-6 col-span-12 mt-4 `}>
+                        <label className="form-label inline-block mb-1 text-amber-600 text-sm font-bold">NOTICE<span className='text-red-500'>*</span></label>
+                        <div className="w-full overflow-auto text-sm py-2 font-mono text-gray-600  px-2 text-justify border-2 border-dashed border-amber-600 rounded">
+                            <p>Any business that is involved in the selling of tobbacco products, can only take license for <span className='text-sky-400 font-extrabold text-md'> a </span>  year ! </p>
+                        </div>
+                        <div>
+                            <label className="form-label inline-block mb-1 mt-4 text-gray-600 text-sm font-semibold">License For Years<span className='text-red-500'>*</span></label>
+                            <select name="licenseForYears" id="" className={`${inputStyle} uppercase`}>
+                                <option value="1">1</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className={`${tocStatusToggle ? 'hidden' : ''} form-group mb-4 md:mb-6 col-span-12 mt-4 `}>
+                        <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">License For Years<span className='text-red-500'>*</span></label>
+                        <div className="w-full overflow-auto">
+                            <select name="licenseForYears" id="" className={`${inputStyle} uppercase`}>
+                                <option value="">select</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                            </select>
                         </div>
 
                     </div>
@@ -44,6 +86,8 @@ function Screen13(props) {
                 </div>
 
             </div>
+
+            <Info infoText="In Case of tobacco the license can be taken only for a year !" />
         </>
     )
 }
---
+++ src/Pages/Trade/NewApplication/TradeApiRequirement.md
@@ -0,0 +1,20 @@
+Trade Api Requirements
+============================================================================
+
+For Citizen
+----------------------------------------------------------------------------
+<!-- File new complaint -->
+New Application
+    ApiList
+        ├──getWardNo
+        ├──getNewWardNo
+        ├──getNoticeDetails
+        ├──getNatureOfBusiness
+        |──get Collective Data as master data for trade form fillup data
+        ├──getPropertyDetails
+        └──posthandleNewApplication
+        └──posthandlePayment
+        |__postHandleDocUpload
+        |__getAllApplicationList
+   
+
