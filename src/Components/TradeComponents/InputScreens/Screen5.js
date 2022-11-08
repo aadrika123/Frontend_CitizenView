e0aec95d4ba74f8e81e0430c793217d67d89e6f0
AnshumanG20 <faaltuno9@gmail.com>
2022-11-08 19:30:59 +0530

    pushed on 08/11/2022

--- src/Components/TradeComponents/InputScreens/Screen5.js
+++ src/Components/TradeComponents/InputScreens/Screen5.js
@@ -14,48 +14,82 @@ import Info from '../../Common/Info'
 import CommonStyles from '../../IndividualRoutes/CommonStyles'
 //importing Themestyle function to use predefined colors to maintain uniform theme everywhere
 import ThemeStyle from '../../Styles/ThemeStyle'
+import * as yup from 'yup';
+import { useFormik } from 'formik'
 
 function Screen5(props) {
     //destructuring predefined colors to maintain uniform theme everywhere
     const { bgHeaderColor, titleColor, nextButtonColor, nextBtnHoverColor, backButtonColor, backBtnHoverColor, bgCardColor, bgInfoColor, infoTextColor } = ThemeStyle()
-
     const { labelStyle, inputStyle } = CommonStyles();
+
+    const { allFormDataFun } = props.values;
+
+    const initialValues = {
+        firmType: ''
+    }
+
+    const validationSchema = yup.object(
+        { firmType: yup.string().required("This field is required !") }
+    );
+
+    const formik = useFormik({
+        initialValues: initialValues,
+
+        onSubmit: (values) => {
+            // alert(JSON.stringify(values));
+            allFormDataFun("firmType",values);
+            props.nextFun();
+        },
+        validationSchema
+    });
+
+    const handleOnChange = (e) => {
+        let name = e.target.name;
+        let vals = e.target.value;
+
+        console.log("name : " + name + "values : " + vals);
+        // { name === 'applyWith' && (vals == 1 ? setnoticeToggle(true) : setnoticeToggle(false)) }
+    }
     return (
         <>
             <div className='text-xs font-semibold px-2 mt-4 flex'>
                 <div className="flex-1"><span onClick={props.backFun} className='border-b border-black'><RiArrowDropLeftFill className="inline text-xl" />Back</span></div>
                 <div className="flex-1 text-right"><span className=''>{props?.formIndex} of 10</span></div>
             </div>
-            <div className='p-2 md:p-10 flex justify-center items-center  overflow-hidden'>
-                <div className={`grid grid-cols-12 ${bgCardColor} shadow-lg w-full md:w-1/3 p-4 py-10 md:p-10`}>
-                    <div className="col-span-12"> <h1 className={`font-bold ${titleColor} text-2xl`}>Firm Type</h1></div>
-                    <div className="col-span-12"> <h1 className={` ${titleColor} text-xs opacity-40`}>Describe the type of your firm from the given below list.</h1></div>
-                    {/* <div className="form-group mb-4 md:mb-6 col-span-12 mt-4">
+
+            <form onSubmit={formik.handleSubmit} onChange={handleOnChange}>
+                <div className='p-2 md:p-10 flex justify-center items-center  overflow-hidden'>
+                    <div className={`grid grid-cols-12 ${bgCardColor} shadow-lg w-full md:w-1/3 p-4 py-10 md:p-10`}>
+                        <div className="col-span-12"> <h1 className={`font-bold ${titleColor} text-2xl`}>Firm Type</h1></div>
+                        <div className="col-span-12"> <h1 className={` ${titleColor} text-xs opacity-40`}>Describe the type of your firm from the given below list.</h1></div>
+                        {/* <div className="form-group mb-4 md:mb-6 col-span-12 mt-4">
                         <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Enter a valid holding no to get property details<span className='text-red-500'>*</span></label>
                     </div> */}
-                    <div className="form-group mb-4 md:mb-6 col-span-12 mt-4 ">
-                        <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Firm Type.<span className='text-red-500'>*</span></label>
-                        <select name='firmType' className={`${inputStyle} uppercase`} placeholder='Search Your Holding No.' >
-                            <option value="">SELECT</option>
-                            <option value="1">Proprietorship</option>
-                            <option value="">Partnership</option>
-                            <option value="">pvt. ltd.</option>
-                            <option value="">public ltd.</option>
-                            <option value="">other</option>
-                        </select>
+                        <div className="form-group mb-4 md:mb-6 col-span-12 mt-4 ">
+                            <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Firm Type.<span className='text-red-500'>*</span></label>
+                            <select name='firmType' className={`${inputStyle} uppercase`} placeholder='Search Your Holding No.' onChange={formik.handleChange} value={formik.values.firmType}>
+                                <option value="">SELECT</option>
+                                <option value="1">Proprietorship</option>
+                                <option value="">Partnership</option>
+                                <option value="">pvt. ltd.</option>
+                                <option value="">public ltd.</option>
+                                <option value="">other</option>
+                            </select>
 
+                            <p className='text-red-500 text-xs'>{formik.touched.firmType && formik.errors.firmType ? formik.errors.firmType : null}</p>
+                        </div>
+                        {/* //////////// */}
+
+
+                        {/* <div className="col-span-12 text-amber-600 font-semibold mt-4"><BiAddToQueue className="inline" /> Add Floor</div> */}
+                        <div className="col-span-12 grid grid-cols-12 gap-x-6 mt-6">
+                            <div className="col-span-6"> <button onClick={() => props.backFun()} type="button" className={`shadow-lg w-full px-6 py-4 ${backButtonColor} text-white font-medium text-xs leading-tight  rounded  ${backBtnHoverColor} hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`}>Back</button></div>
+                            <div className="col-span-6"> <button type="submit" className={`shadow-lg w-full px-6 py-4 ${nextButtonColor} text-white font-medium text-xs leading-tight  rounded  ${nextBtnHoverColor} hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`}>Next</button></div>
+                        </div>
                     </div>
-                    {/* //////////// */}
 
-
-                    {/* <div className="col-span-12 text-amber-600 font-semibold mt-4"><BiAddToQueue className="inline" /> Add Floor</div> */}
-                    <div className="col-span-12 grid grid-cols-12 gap-x-6 mt-6">
-                        <div className="col-span-6"> <button onClick={() => props.backFun()} type="submit" className={`shadow-lg w-full px-6 py-4 ${backButtonColor} text-white font-medium text-xs leading-tight  rounded  ${backBtnHoverColor} hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`}>Back</button></div>
-                        <div className="col-span-6"> <button onClick={() => props.nextFun()} type="submit" className={`shadow-lg w-full px-6 py-4 ${nextButtonColor} text-white font-medium text-xs leading-tight  rounded  ${nextBtnHoverColor} hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`}>Next</button></div>
-                    </div>
                 </div>
-
-            </div>
+            </form>
             <Info infoText="Selecting correct type of the firm will lead to the correct evaluation of the firm and will help you ease up the process in the longer run." />
         </>
     )
--- src/Components/TradeComponents/InputScreens/Screen9.js
+++ src/Components/TradeComponents/InputScreens/Screen9.js
@@ -14,65 +14,114 @@ import Info from '../../Common/Info'
 import CommonStyles from '../../IndividualRoutes/CommonStyles'
 //importing Themestyle function to use predefined colors to maintain uniform theme everywhere
 import ThemeStyle from '../../Styles/ThemeStyle'
+import * as yup from 'yup';
+import { useFormik } from 'formik'
 
 function Screen9
     (props) {
     //destructuring predefined colors to maintain uniform theme everywhere
     const { bgHeaderColor, titleColor, nextButtonColor, nextBtnHoverColor, backButtonColor, backBtnHoverColor, bgCardColor, bgInfoColor, infoTextColor } = ThemeStyle()
+    const { labelStyle, inputStyle } = CommonStyles();
 
-    const { labelStyle, inputStyle } = CommonStyles();  
+    const { allFormDataFun } = props.values;
+
+    const initialValues = {
+        holdingNo: '',
+        premisesOwner: '',
+        businessAddress: '',
+        landmark: '',
+        pincode: '',
+    }
+
+    const validationSchema = yup.object(
+        {
+            holdingNo: yup.string().required("This field is required !"),
+            premisesOwner: yup.string().required("This field is required !"),
+            businessAddress: yup.string().required("This field is required !"),
+            landmark: yup.string().required("This field is required !"),
+            pincode: yup.string().required("This field is required !"),
+        }
+    );
+
+    const formik = useFormik({
+        initialValues: initialValues,
+
+        onSubmit: (values) => {
+            // alert(JSON.stringify(values));
+            allFormDataFun("propertyDetails",values);
+            props.nextFun();
+        },
+        validationSchema
+    });
+
+    const handleOnChange = (e) => {
+        let name = e.target.name;
+        let vals = e.target.value;
+
+        console.log("name : " + name + "values : " + vals);
+        // { name === 'applyWith' && (vals == 1 ? setnoticeToggle(true) : setnoticeToggle(false)) }
+    }
+
     return (
         <>
             <div className='text-xs font-semibold px-2 mt-4 flex'>
                 <div className="flex-1"><span onClick={props.backFun} className='border-b border-black'><RiArrowDropLeftFill className="inline text-xl" />Back</span></div>
                 <div className="flex-1 text-right"><span className=''>{props?.formIndex} of 10</span></div>
             </div>
-            <div className='p-2 md:p-10 flex justify-center items-center  overflow-hidden'>
-                <div className={`grid grid-cols-12 ${bgCardColor} shadow-lg w-full md:w-1/3 p-4 py-10 md:p-10`}>
-                    <div className="col-span-12"> <h1 className={`font-bold ${titleColor} text-2xl`}>Property Details</h1></div>
-                    <div className="col-span-12"> <h1 className={` ${titleColor} text-xs opacity-40`}>Enter a valid holding number to get property details</h1></div>
 
-                    <div className="form-group mb-4 md:mb-6 col-span-12 mt-4">
-                        <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Holding No.<span className='text-red-500'>*</span></label>
-                        <input type="text" name='holdingNo' className={`${inputStyle}`} placeholder='Search Your Holding No.' />
+            <form onSubmit={formik.handleSubmit} onChange={handleOnChange}>
+                <div className='p-2 md:p-10 flex justify-center items-center  overflow-hidden'>
+                    <div className={`grid grid-cols-12 ${bgCardColor} shadow-lg w-full md:w-1/3 p-4 py-10 md:p-10`}>
+                        <div className="col-span-12"> <h1 className={`font-bold ${titleColor} text-2xl`}>Property Details</h1></div>
+                        <div className="col-span-12"> <h1 className={` ${titleColor} text-xs opacity-40`}>Enter a valid holding number to get property details</h1></div>
 
+                        <div className="form-group mb-4 md:mb-6 col-span-12 mt-4">
+                            <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Holding No.<span className='text-red-500'>*</span></label>
+                            <input type="text" name='holdingNo' className={`${inputStyle}`} placeholder='Search Your Holding No.' onChange={formik.handleChange} value={formik.values.holdingNo} />
+
+                            <p className='text-red-500 text-xs'>{formik.touched.holdingNo && formik.errors.holdingNo ? formik.errors.holdingNo : null}</p>
+
+                        </div>
+                        {/* //////////// */}
+
+                        <div className={`form-group mb-4 md:mb-6 col-span-12 `}>
+                            <div className="form-group mb-4 md:mb-6 col-span-12 ">
+                                <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Owner of Business Premises.<span className='text-red-500'>*</span></label>
+                                <input type="text" name='premisesOwner' className={`${inputStyle}`} placeholder='Name of the Premises Owner.' onChange={formik.handleChange} value={formik.values.premisesOwner} />
+                                <p className='text-red-500 text-xs'>{formik.touched.premisesOwner && formik.errors.premisesOwner ? formik.errors.premisesOwner : null}</p>
+
+                            </div>
+
+                            <div className="form-group mb-4 md:mb-6 col-span-12">
+                                <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Business Address.<span className='text-red-500'>*</span></label>
+                                <input type="text" name='businessAddress' className={`${inputStyle}`} placeholder='Enter Business Address.' onChange={formik.handleChange} value={formik.values.businessAddress} />
+                                <p className='text-red-500 text-xs'>{formik.touched.businessAddress && formik.errors.businessAddress ? formik.errors.businessAddress : null}</p>
+
+                            </div>
+                            <div className="form-group mb-4 md:mb-6 col-span-12 ">
+                                <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Landmark<span className='text-red-500'>*</span></label>
+                                <input type="text" name='landmark' className={`${inputStyle}`} placeholder='Enter Landmark.' onChange={formik.handleChange} value={formik.values.landmark} />
+                                <p className='text-red-500 text-xs'>{formik.touched.landmark && formik.errors.landmark ? formik.errors.landmark : null}</p>
+
+                            </div>
+
+                            <div className="form-group mb-4 md:mb-6 col-span-12 ">
+                                <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Pincode<span className='text-red-500'>*</span></label>
+                                <input type="text" name='pincode' className={`${inputStyle}`} placeholder='Enter Pincode.' onChange={formik.handleChange} value={formik.values.pincode} />
+                                <p className='text-red-500 text-xs'>{formik.touched.pincode && formik.errors.pincode ? formik.errors.pincode : null}</p>
+
+                            </div>
+                        </div>
+
+                        {/* <div className="col-span-12 text-amber-600 font-semibold "><BiAddToQueue className="inline" /> Add Floor</div> */}
+                        <div className="col-span-12 grid grid-cols-12 gap-x-6 mt-6">
+                            <div className="col-span-6"> <button onClick={() => props.backFun()} type="button" className={`shadow-lg w-full px-6 py-4 ${backButtonColor} text-white font-medium text-xs leading-tight  rounded  ${backBtnHoverColor} hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`}>Back</button></div>
+                            <div className="col-span-6"> <button  type="submit" className={`shadow-lg w-full px-6 py-4 ${nextButtonColor} text-white font-medium text-xs leading-tight  rounded  ${nextBtnHoverColor} hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`}>Next</button></div>
+                        </div>
                     </div>
-                    {/* //////////// */}
 
-                    <div className={`form-group mb-4 md:mb-6 col-span-12 `}>
-                        <div className="form-group mb-4 md:mb-6 col-span-12 ">
-                            <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Owner of Business Premises.<span className='text-red-500'>*</span></label>
-                            <input type="text" name='premisesOwner' className={`${inputStyle}`} placeholder='Name of the Premises Owner.' />
-
-                        </div>
-
-                        <div className="form-group mb-4 md:mb-6 col-span-12">
-                            <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Business Address.<span className='text-red-500'>*</span></label>
-                            <input type="text" name='businessAddress' className={`${inputStyle}`} placeholder='Enter Business Address.' />
-
-                        </div>
-                        <div className="form-group mb-4 md:mb-6 col-span-12 ">
-                            <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Landmark<span className='text-red-500'>*</span></label>
-                            <input type="text" name='landmark' className={`${inputStyle}`} placeholder='Enter Landmark.' />
-
-                        </div>
-
-                        <div className="form-group mb-4 md:mb-6 col-span-12 ">
-                            <label className="form-label inline-block mb-1 text-gray-600 text-sm font-semibold">Pincode<span className='text-red-500'>*</span></label>
-                            <input type="text" name='pincode' className={`${inputStyle}`} placeholder='Enter Pincode.' />
-
-                        </div>
-                    </div>
-
-                    {/* <div className="col-span-12 text-amber-600 font-semibold "><BiAddToQueue className="inline" /> Add Floor</div> */}
-                    <div className="col-span-12 grid grid-cols-12 gap-x-6 mt-6">
-                        <div className="col-span-6"> <button onClick={() => props.backFun()} type="submit" className={`shadow-lg w-full px-6 py-4 ${backButtonColor} text-white font-medium text-xs leading-tight  rounded  ${backBtnHoverColor} hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`}>Back</button></div>
-                        <div className="col-span-6"> <button onClick={() => props.nextFun()} type="submit" className={`shadow-lg w-full px-6 py-4 ${nextButtonColor} text-white font-medium text-xs leading-tight  rounded  ${nextBtnHoverColor} hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out`}>Next</button></div>
-                    </div>
                 </div>
-
-            </div>
-
+            </form>
 
             <Info infoText="To map your premises Property Details, enter your holding no in the given field above. It will fill relevant details in the input fields." />
         </>
