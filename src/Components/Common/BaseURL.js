e3d124e14636959a0dda04386ef445ddf9cc5314
Dipu Singh <dsingh197@gmail.com>
2022-11-11 14:01:04 +0530

    update

--- src/Components/Common/BaseURL.js
+++ src/Components/Common/BaseURL.js
@@ -1,7 +1,11 @@
 export default function BaseURL() {
-    
+
+
     let BaseURL = {
-         baseUrl : "http://192.168.0.16:8000",
+        // baseUrl: "http://192.168.0.148:81"
+        baseUrl: "http://192.168.0.16:8000",
+        
+        bearer: "1478|vGZADnTinTHsaluolnVXrh5wQiR2PaIJeAMzBz9k",
     }
 
     return BaseURL
